6d64d6eb4052ecb648b900730f061d8cde8c7404
Chris Frisz <chris.frisz@gmail.com>
2013-08-18 21:26:20 -0400

    Adding simpler set-based map generation algorithm.

--- src/cljs/chaser_cljs/map_generator.cljs
+++ src/cljs/chaser_cljs/map_generator.cljs
@@ -7,102 +7,43 @@
 ;; Map generation functions.
 ;;--------------------------------------------------
 
-(ns chaser-cljs.map-generator)
+(ns chaser-cljs.map-generator
+  (:require [clojure.set :refer (union difference)]))
 
-(def dir* [:left :right :up :down])
+(defn make-coords [x y] (vector x y))
+(def coords-get-x first)
+(def coords-get-y fnext)
+(defn coords-update-x [coords new-x] (assoc coords 0 new-x))
+(defn coords-update-y [coords new-y] (assoc coords 1 new-y))
 
-;;------------------------------
-;; Map spaces
-;;------------------------------
-(defn make-space
-  "Returns a new space with the given x and y attributes."
-  [x y]
-  {:x x :y y})
+(defn adjacent-coords
+  [coords]
+  (let [x (coords-get-x coords)
+        y (coords-get-y coords)]
+    #{(make-coords x (inc y))    ;; up
+      (make-coords (inc x) y)    ;; right
+      (make-coords x (dec y))    ;; down
+      (make-coords (dec x) y)})) ;; left
 
-(def space-get-x :x)
-
-(def space-get-y :y)
-
-;; NB: uncomfortable that this returns undecorated vector, but
-;;     returning as a map isn't useful
-(defn get-adjacent-coords
-  "Returns a vector of the x and y values, respectively, for the space
-   given by dir. Valid values for dir:
-       :left
-       :right
-       :up
-       :down"
-  [space dir]
-  (let [x (space-get-x space)
-        y (space-get-y space)]
-    (case dir
-      :left  [(dec x) y]
-      :right [(inc x) y]
-      :up    [x (inc y)]
-      :down  [x (dec y)]
-      :else  (throw (js/Error (str "invalid dir argument " dir))))))
-
-;;----------------------------------------
-;; Map definition, accessors, updaters
-;;----------------------------------------
-(def make-game-map hash-map)
-
-(defn game-map-get-space
-  "Returns the space within the game-map given by the x and y values."
-  [game-map x y]
-  (get-in game-map [x y]))
-
-(defn game-map-update-space
-  "Updates the space within the game-map given by the x and y values of
-   the space to match the given space."
-  [game-map space]
-  (assoc-in game-map [(space-get-x space) (space-get-y space)] space))
-
-;;----------------------------------------
-;; Map builder
-;;----------------------------------------
-(defn expand-around-space
-  "Returns a vector representing a randomized (not necessarily proper)
-   subset of unoccupied spaces cardinally adjacent to the given space
-   according to game-map."
-  [game-map space]
-  (remove false?
-    (for [dir (shuffle dir*)
-          :let [[adj-x adj-y] (get-adjacent-coords space dir)]]
-      (and (nil? (game-map-get-space game-map adj-x adj-y))
-           (= (rand-int 2) 1)
-           (make-space adj-x adj-y)))))
-
-(defn num-surrounding-spaces
-  "Returns the number of spaces cardinally adjacent to space according
-   to game-map."
-  [game-map space]
-  (reduce (fn [total dir]
-            (if (nil? (apply game-map-get-space
-                        game-map
-                        (get-adjacent-coords space dir)))
-                total
-                (inc total)))
-    0
-    dir*))
+(defn normalize-coords
+  [coords*]
+  (let [min-x (apply min (map coords-get-x coords*))
+        min-y (apply min (map coords-get-y coords*))]
+    (if (and (zero? min-x) (zero? min-y))
+        (vec coords*)
+        (mapv #(as-> % coords
+                 (coords-update-x coords (- (coords-get-x coords) min-x))
+                 (coords-update-y coords (- (coords-get-y coords) min-y)))
+          coords*))))
 
 (defn build-map
-  "Returns a procedurally-generated game-map of with target-size number
-   of spaces. It generates the map by starting from a single space at
-   (0, 0), and for each space with at least one unused adjacent space,
-   there is a 50% chance that a new space is added. The algorithm
-   continues until the target-size is reached."
   [target-size]
-  (let [init-space (make-space 0 0)]
-    (loop [game-map (game-map-update-space (make-game-map) init-space)
-           work-list* [init-space]
-           remaining-spaces (dec target-size)]
-      (if (zero? remaining-spaces)
-          game-map
-          (let [new-space* (take (min remaining-spaces (count dir*))
-                             (expand-around-space game-map (first work-list*)))]
-            (recur (reduce game-map-update-space game-map new-space*)
-              (shuffle (remove (comp (partial = 4)
-                                     (partial num-surrounding-spaces game-map))
-                         (concat work-list* new-space*)))
-              (- remaining-spaces (count new-space*))))))))
+  (let [origin (make-coords 0 0)]
+    (loop [coords* #{origin}
+           adjacent* (adjacent-coords origin)]
+      (if (= (count coords*) target-size)
+          (normalize-coords coords*)
+          (let [new-coords (rand-nth adjacent*)]
+            (recur (conj coords* new-coords)
+              (union (difference (adjacent-coords new-coords) coords*)
+                (disj adjacent* new-coords))))))))
