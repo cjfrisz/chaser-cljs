56fc733154317425824d4352cd24913358a50905
Chris Frisz <chris.frisz@gmail.com>
2013-09-02 16:52:03 -0400

    Updating project file with latest CLJS version.

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
     :name "Eclipse Public License"
     :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1859"]
                  [ring "1.1.8"]
                  [prismatic/dommy "0.1.1"]]
-  :plugins [[lein-cljsbuild "0.3.2"]
+  :plugins [[lein-cljsbuild "0.3.3-SNAPSHOT"]
             [lein-ring "0.8.3"]]
-  :source-paths ["src/clj"]
+  :source-paths ["src/clj" "src/cljs"]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {
     :builds 
