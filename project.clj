1d605deef86c56cf185263d873144822feeab1c6
Chris Frisz <chris.frisz@gmail.com>
2013-09-11 09:51:52 -0400

    Attempting to type-check with newer version of core.typed.

--- project.clj
+++ project.clj
@@ -7,11 +7,14 @@
                  [org.clojure/clojurescript "0.0-1859"]
                  [ring "1.1.8"]
                  [prismatic/dommy "0.1.1"]
-                 [org.clojure/core.typed "0.2.1"]]
+                 [org.clojure/core.typed "0.2.5-20130907.135349-4"]]
   :plugins [[lein-cljsbuild "0.3.3-SNAPSHOT"]
-            [lein-ring "0.8.3"]]
+            [lein-ring "0.8.3"]
+            [lein-typed "0.3.0"]]
   :source-paths ["src/clj" "src/cljs"]
   :hooks [leiningen.cljsbuild]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :core.typed {:check [chaser-cljs.coords]}
   :cljsbuild {
     :builds 
       [{:source-paths ["src/cljs"],
--- src/cljs/chaser_cljs/coords.cljs
+++ src/cljs/chaser_cljs/coords.cljs
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 18 Aug 2013
-;; Last modified  2 Sep 2013
+;; Last modified 10 Sep 2013
 ;; 
 ;; Simple coordinate representation.
 ;;----------------------------------------------------------------------
@@ -11,6 +11,25 @@
 (ns chaser-cljs.coords
   (:require-macros [cljs.core.typed :refer (ann def-alias)]))
 
+(ann ^:no-check cljs.core/assoc number)
+(def-alias EmptySeqable (TFn [[x :variance :covariant]] (I (cljs.core/ISeqable x) (ExactCount 0))))
+(def-alias NonEmptySeqable (TFn [[x :variance :covariant]] (I (cljs.core/ISeqable x) (CountRange 1))))
+(ann ^:no-check cljs.core/vector (All [x y z a b c] 
+                       (Fn
+                        [-> '[]]
+                        [x -> '[x]]
+                        [x y -> '[x y]]
+                        [x y z -> '[x y z]]
+                        [x y z a -> '[x y z a]]
+                        [x y z a b -> '[x y z a b]]
+                        [x y z a b c -> '[x y z a b c]]
+                        [x * -> (cljs.core/IVector x)])))
+(ann ^:no-check cljs.core/first (All [x]
+                (Fn [(U nil (EmptySeqable x)) -> nil]
+                    [(NonEmptySeqable x) -> x]
+                    [(U nil (cljs.core/ISeqable x)) -> (U nil x)])))
+(ann ^:no-check cljs.core/fnext (All [x]
+                [(U nil (cljs.core/ISeqable (U nil (cljs.core/ISeqable x)))) -> (U nil x)]))
 ;; NB: this is entirely equivalent to (def make-coords vector) for 
 ;;     ClojureScript since there's no arity enforcement, but it would
 ;;     feel dirty not to do it this way.
@@ -24,20 +43,27 @@
   ;; looks too weird
   (vector x y))
 
-(ann get-x [CoordsT -> CoordValT])
-(def ^{:doc "Returns the x value for the given coords structure."}
+(ann ^:no-check get-x [CoordsT -> CoordValT])
+(defn get-x [[x y]] x)
+#_(def ^{:doc "Returns the x value for the given coords structure."}
   get-x first)
-(ann get-y [CoordsT -> CoordValT])
-(def ^{:doc "Returns the y value for the given coords structure."}
+(ann ^:no-check get-y [CoordsT -> CoordValT])
+(defn get-y [[x y]] y)
+#_(def ^{:doc "Returns the y value for the given coords structure."}
   get-y fnext)
 
-(ann update-x [CoordsT CoordValT -> CoordsT])
-(defn update-x 
+(ann ^:no-check update-x [CoordsT CoordValT -> CoordsT])
+(defn update-x [[x y] new-x] [new-x y])
+#_(defn update-x 
   "Updates the x value for the given coords structure to be new-x."
   [coords new-x]
   (assoc coords 0 new-x))
 (ann update-y [CoordsT CoordValT -> CoordsT])
+(defn update-y [[x y] new-y] [x new-y])
 (defn update-y 
   "Updates the x value for the given coords structure to be new-x."
   [coords new-y]
   (assoc coords 1 new-y))
+
+(ann update-first [(Vector* number number) number -> (Vector* number number)])
+(defn update-first [my-vec new-first] (assoc my-vec 1 new-first))
